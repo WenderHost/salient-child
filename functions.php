96c9b2fb56efee600d0414d39d182b98fd93a69d
Michael Wender <michael@michaelwender.com>
2017-04-12 11:03:03 -0400

    Adding JS injected breadcrumbs

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
 // Include files
+include_once 'lib/fns/breadcrumbs.php';
+include_once 'lib/fns/debug.php';
 include_once 'lib/fns/plugins.php';
 include_once 'lib/fns/tinymce.php';
 
-add_action( 'wp_enqueue_scripts', 'salient_parent_theme_enqueue_styles', 99 );
+/**
+ * Enqueues parent theme styles
+ */
 function salient_parent_theme_enqueue_styles() {
     $deregister_styles = ['salient-style','rgs','font-awesome','main-styles','responsive','skin-ascend'];
     foreach ($deregister_styles as $handle ) {
@@ -12,6 +16,11 @@ function salient_parent_theme_enqueue_styles() {
     $version = ( file_exists( get_stylesheet_directory() . '/lib/css/main.css' ) ) ? filemtime( get_stylesheet_directory() . '/lib/css/main.css' ) : '';
     wp_enqueue_style( 'salient-child-style', get_stylesheet_directory_uri() . '/lib/css/main.css', null, $version );
 
-}
+    wp_enqueue_script( 'global', get_stylesheet_directory_uri() . '/lib/js/global.js', ['jquery','nectarFrontend'], filemtime( get_stylesheet_directory() . '/lib/js/global.js' ), true );
+    wp_localize_script( 'global', 'wpvars', ['breadcrumbs' => SalientChild\fns\breadcrumbs\get_breadcrumbs() ] );
 
+}
+add_action( 'wp_enqueue_scripts', 'salient_parent_theme_enqueue_styles', 99 );
+
+// Enqueue TinyMCE Editor styles
 add_editor_style( get_stylesheet_directory_uri() . '/lib/css/editor-styles.css' );
